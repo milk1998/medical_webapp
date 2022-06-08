6244529f2f5b7a10dc9351891043b04850a87d27
milk1998 <macmac030998@gmail.com>
2022-06-08 12:44:52 +0000

    add new commit

---
+++ Public/Body/Header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="Body/Css/Header.css">
+	<link rel="stylesheet" href="Body/Css/Main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.3/font/bootstrap-icons.min.css" integrity="sha512-YzwGgFdO1NQw1CZkPoGyRkEnUTxPSbGWXvGiXrWk8IeSqdyci0dEDYdLLjMxq1zCoU0QBa4kHAFiRhUL3z2bow==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Sen Boutique Sandaga chez vous</title>
+</head>
+<body>
+    <header>
+        <nav class="nav-bar">
+            <div class="nav-item nav-item1">
+                <a href="#" class="nav-a-link"><img src="" alt="Logo Sandaga chez vous"></a>
+            </div>
+            <div class="nav-item">
+                <form action="" class="nav-form" method="post">
+                    <input type="search" class="nav-search-input" name="" id="">
+                    <input type="button" class="nav-search-button" value="Rechercher">
+                </form>
+            </div>
+            <div class="nav-item nav-item3">
+                <div class="nav-nav">
+                    <div class="nav-nav-item">Panier<i class="bi bi-cart-fill"><sup>0</sup></i></div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
